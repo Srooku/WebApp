88682e6b2e1d00943e602db2d8129f6e853df226
Srooku <107481257+Srooku@users.noreply.github.com>
2022-06-15 10:43:43 +0300

    Test

---
+++ Models/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime DateBirth { get; set; }
+        public DateTime DateDeath { get; set; }
+
+        public Author()
+        {
+
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Book()
+        {
+
+        }
+    }
+}
